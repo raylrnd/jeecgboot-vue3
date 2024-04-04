40a5db405de1f56b60199217aa74ef7d46a97419
zhongshunchao <382526825@qq.com>
2024-04-04 15:50:52 +0800

    author

--- src/views/salary/SalaryCalculate.vue
+++ src/views/salary/SalaryCalculate.vue
@@ -1,23 +1,25 @@
 <template>
-  <div>
-    <Select v-model="selectedMonthYear" :style="{ width: '150px' }" :placeholder="placeholderText">
-      <option v-for="option in options" :value="option.value" :key="option.value">
-        {{ option.text }}
-      </option>
-    </Select>
-    <a-button @click="calculateSalary">计算工资</a-button>
-  </div>
+  <BasicModal>
+    <div style="margin: 10px 20px 20px">
+      <Select v-model="selectedMonthYear" :style="{ width: '150px' }" :placeholder="placeholderText">
+        <option v-for="option in options" :value="option.value" :key="option.value">
+          {{ option.text }}
+        </option>
+      </Select>
+      <a-button type="primary" @click="calculateSalary" style="margin: 10px 20px 20px">计算工资 </a-button>
+    </div>
+  </BasicModal>
 </template>
 
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue';
 import { Select } from 'ant-design-vue';
 import { getSalary } from './SalaryCalculate.api';
+import { message } from 'ant-design-vue';
 
 const currentYearMonth = new Date().toISOString().slice(0, 7);
 const selectedMonthYear = ref(currentYearMonth);
 const options = ref([]);
-
 
 function generatePast50YearsOptions() {
   const options = [];
@@ -49,6 +51,11 @@ async function calculateSalary() {
     console.log('No month selected');
     return;
   }
-  await getSalary(selectedMonthYear.value);
+  try {
+    const result = await getSalary(selectedMonthYear.value);
+    message.success(result.result);
+  } catch (error) {
+    message.error(error.message);
+  }
 }
 </script>
