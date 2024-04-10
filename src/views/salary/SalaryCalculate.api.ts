04f048b98c2e4928325146b55650a94647ce7c05
zhongshunchao <382526825@qq.com>
2024-04-10 09:55:59 +0800

    author

--- src/views/salary/SalaryCalculate.api.ts
+++ src/views/salary/SalaryCalculate.api.ts
@@ -8,7 +8,7 @@ export async function getSalary(yearmonth: string) {
   let token = getToken();
 
   //将这里的URL替换为你实际的后端地址即可
-  const url = `http://jeecg-boot-system:8080/jeecg-boot/salary/getCalculate?yearmonth=${yearmonth}`;
+  const url = `http://www.salary.gdhzairport.com:9797/jeecg-boot/salary/getCalculate?yearmonth=${yearmonth}`;
   console.log("Request URL:", url);
 
   try {
